9f6a2438d3187343090273f7965b629c3d75dcd5
Areeb Khan <areeb.khanahmed@gmail.com>
2024-12-10 11:31:13 -0500

    basecode/2

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,6 +3,15 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Paste your firebaseConfig from Firebase Console here
+const firebaseConfig = {
+    apiKey: "AIzaSyD4DW-qAn53NjWeGtQmqRkyIqgHdhOeGsI",
+    authDomain: "summative-e787e.firebaseapp.com",
+    projectId: "summative-e787e",
+    storageBucket: "summative-e787e.firebasestorage.app",
+    messagingSenderId: "9944568923",
+    appId: "1:9944568923:web:3c9ee19f54bea8622adb7e"
+  };
+  
 
 
 const config = initializeApp(firebaseConfig)
